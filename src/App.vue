8a94c53da10289bc8c5c9bbc9686cc7eaec8e7f6
Praseetha-KR <praseetha04@gmail.com>
2018-10-25 01:49:50 +0530

    Replaced hello template with DeviceConnect component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,41 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <header class="siteheader">
+      <img class="siteheader__logo" src="./assets/logo.svg" alt="appknox">
+      <span class="siteheader__separator"> | </span>
+      <span class="siteheader__title">Device Manager</span>
+    </header>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: $color-text;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+.siteheader {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  height: 3.2em;
+  background: $color-bg;
+  box-shadow: 1px 0 12px rgba(lighten($color-text, 45%), 0.5);
+
+  &__separator {
+    padding: 0 0.3em;
+    font-size: 1.8em;
+    color: lighten($color-text, 65%);
+  }
+
+  &__title {
+    font-size: 1.2em;
   }
 }
 </style>
